a0b5fc51b51da8691dafea3ce84ca510c0ba050a
Emeka boris ama <borisphilosophy@gmail.com>
2023-04-20 19:43:58 +0100

    Update api.py

--- api.py
+++ api.py
@@ -74,4 +74,4 @@ def download():
     # return send_from_directory(directory, filepath, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run( host= '0.0.0.0', port = 8501, debug=True)
+    app.run( host= '0.0.0.0', port = 8080, debug=True)
